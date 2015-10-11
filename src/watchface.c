7cea2f91dfd3900ea77c8c6556c299a51f8dadbb
Paul Heasley <paul.heasley@gmail.com>
2015-10-12 08:30:05 +1100

    Added date

--- src/watchface.c
+++ src/watchface.c
@@ -2,36 +2,42 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static GFont s_time_font;
+static GFont s_date_font;
 
-static void update_time() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
-
+static void update_time(struct tm *tick_time) {
   // Create a long-lived buffer
-  static char buffer[] = "00:00";
+  static char time_buffer[] = "00:00";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(time_buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(time_buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
 
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_time_layer, time_buffer);
+}
+
+static void update_date(struct tm *tick_time) {
+  static char date_buffer[] = "MON 00";
+  strftime(date_buffer, sizeof("MON 00"), "%a %e", tick_time);
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  update_time(tick_time);
+  update_date(tick_time);
 }
 
 static void main_window_load(Window *window) {
   window_set_background_color(window, GColorBlack);
 
+  // Create and add the time layer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorWhite);
@@ -41,6 +47,17 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  // Create and add the date layer
+  s_date_layer = text_layer_create(GRect(0, 110, 144, 50));
+  text_layer_set_background_color(s_date_layer, GColorBlack);
+  text_layer_set_text_color(s_date_layer, GColorWhite);
+
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_IMAGINE_20));
+  text_layer_set_font(s_date_layer, s_date_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -61,7 +78,11 @@ static void init() {
   window_stack_push(s_main_window, true);
 
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  update_time();
+
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+  update_time(tick_time);
+  update_date(tick_time);
 }
 
 static void deinit() {
