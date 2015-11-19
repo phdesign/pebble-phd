a68254355e8986355dd355f9991f244d66b7e973
Paul Heasley <paul.heasley@gmail.com>
2015-11-20 08:07:32 +1100

    Translating yahoo weather conditions into shorter text

--- src/js/weather-services/yahoo-weather.js
+++ src/js/weather-services/yahoo-weather.js
@@ -1,7 +1,63 @@
 var utils = require('../utils.js');
 var request = require('request');
 
-var buildRequestOptions = function(lat, lon) {
+function getShortConditionText(conditionCode) {
+  var remap = {
+    '0': 'tornado', //tornado
+    '1': 'storms', //tropical storm
+    '2': 'storms', //hurricane
+    '3': 'storms', //severe thunderstorms
+    '4': 'storms', //thunderstorms
+    '5': 'snow', //mixed rain and snow
+    '6': 'sleet', //mixed rain and sleet
+    '7': 'sleet', //mixed snow and sleet
+    '8': 'drizzle', //freezing drizzle
+    '9': 'drizzle', //drizzle
+    '10': 'rain', //freezing rain
+    '11': 'showers', //showers
+    '12': 'showers', //showers
+    '13': 'snow', //snow flurries
+    '14': 'snow', //light snow showers
+    '15': 'snow', //blowing snow
+    '16': 'snow', //snow
+    '17': 'hail', //hail
+    '18': 'sleet', //sleet
+    '19': 'dust', //dust
+    '20': 'foggy', //foggy
+    '21': 'haze', //haze
+    '22': 'smoky', //smoky
+    '23': 'blustery', //blustery
+    '24': 'windy', //windy
+    '25': 'cold', //cold
+    '26': 'cloudy', //cloudy
+    '27': 'cloudy', //mostly cloudy (night)
+    '28': 'cloudy', //mostly cloudy (day)
+    '29': 'cloudy', //partly cloudy (night)
+    '30': 'cloudy', //partly cloudy (day)
+    '31': 'clear', //clear (night)
+    '32': 'sunny', //sunny
+    '33': 'fair', //fair (night)
+    '34': 'fair', //fair (day)
+    '35': 'hail', //mixed rain and hail
+    '36': 'hot', //hot
+    '37': 'storms', //isolated thunderstorms
+    '38': 'storms', //scattered thunderstorms
+    '39': 'storms', //scattered thunderstorms
+    '40': 'storms', //scattered showers
+    '41': 'snow', //heavy snow
+    '42': 'snow', //scattered snow showers
+    '43': 'snow', //heavy snow
+    '44': 'cloudy', //partly cloudy
+    '45': 'showers', //thundershowers
+    '46': 'snow', //snow showers
+    '47': 'storms', //isolated thundershowers
+    '3200': 'unknown' // not available
+  };
+
+  return remap[conditionCode] || 'unknown';
+}
+
+function buildRequestOptions(lat, lon) {
   return {
     url: 'https://query.yahooapis.com/v1/public/yql',
     //url: 'http://localhost:8080/yahoo-weather.json',
@@ -12,15 +68,15 @@ var buildRequestOptions = function(lat, lon) {
     json: true,
     verbose: true
   };
-};
+}
 
-var readWeatherValues = function(rawData) {
+function readWeatherValues(rawData) {
   var celsius = utils.fahreheitToCelsius(rawData.query.results.channel.item.condition.temp);
   return {
     temp: Math.round(celsius),
-    conditions: rawData.query.results.channel.item.condition.text
+    conditions: getShortConditionText(rawData.query.results.channel.item.condition.code)
   };
-};
+}
 
 module.exports = {
 
--- test/js/specs/yahoo-weather.spec.js
+++ test/js/specs/yahoo-weather.spec.js
@@ -1,25 +1,51 @@
 var request = require('request');
 var api = require('../../../src/js/weather-services/yahoo-weather.js');
-var sampleResponse = require('../fixtures/yahoo-weather.json');
 
 describe('Yahoo Weather API', function() {
+  var sampleResponse;
+  var coords;
 
   beforeEach(function() {
+    // Load sampleResponse each time so tests can alter it
+    sampleResponse = require('../fixtures/yahoo-weather.json');
+    coords = { latitude: 0, longitude: 0 };
+
     request.and.callFake(function(options, callback) {
       callback(null, {}, sampleResponse);
     });
   });
 
   it('should return a valid temperature and conditions given a valid request', function(done) {
-
-    var coords = { latitude: 0, longitude: 0 };
-
     api.getCurrentConditions(coords, function(values) {
       expect(values.temp).toBe(16);
-      expect(values.conditions).toBe('Mostly Cloudy');
+      expect(values.conditions).toBe('cloudy');
       done();
     });
+  });
 
+  it('should return \'unknown\' if condition code is not recognised', function(done) {
+    sampleResponse.query.results.channel.item.condition.code = -1;
+
+    api.getCurrentConditions(coords, function(values) {
+      expect(values.conditions).toBe('unknown');
+      done();
+    });
+  });
+
+  it('should not display conditions text longer than 9 characters', function(done) {
+    var testNext = function(i) {
+      sampleResponse.query.results.channel.item.condition.code = i;
+      api.getCurrentConditions(coords, function(values) {
+        expect(values.conditions.length).toBeLessThan(9);
+
+        if (i < 47)
+          testNext(i + 1);
+        else
+          done();
+      });
+    };
+
+    testNext(0);
   });
 
 });
