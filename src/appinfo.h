da11b357121b19aae2dde2979ad3073783f7f62a
Paul Heasley <paul.heasley@gmail.com>
2015-11-27 07:11:10 +1100

    Released version 1.2

--- src/appinfo.h
+++ src/appinfo.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LONG_NAME "PHD"
-#define VERSION_LABEL "1.1"
+#define VERSION_LABEL "1.2"
 #define UUID "ffb5b31d-a0e7-4452-9592-d1ce204b1191"
 
 #define PERSIST_KEY_CONFIG 0
